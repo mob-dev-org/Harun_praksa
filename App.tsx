8be2b0afba4ceb4c18bccdc1b4e9671531f703db
harunXh <husejnovic.harun@gmail.com>
2023-02-06 16:34:35 +0100

    feat(arrays): adding status bar, more examples

--- App.tsx
+++ App.tsx
@@ -7,6 +7,13 @@ import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
 
 export default function App() {
+    const numbers: number[] = [1, 2, 3, 4]; // [1, 2, 3, 4, 5]
+    numbers.push(5); // add element
+    // numbers.push('Harun'); string is not assignable to parametar of type number
+    //
+    const names: string[] = ['Harun', ' ', 'Bajram', ' ', 'Mahir', ' ', 'Malik', ' ', 'Almin'];
+    let lenght;
+    names.pop(); // remove last element
     const isLoadingComplete = useCachedResources();
     const colorScheme = useColorScheme();
 
@@ -15,11 +22,12 @@ export default function App() {
     } else {
         return (
             <View style={[{ alignItems: 'center', justifyContent: 'center', height: '50%' }]}>
-                <Text>TEST VARIJABLE</Text>
-                <Text>TEST VARIJABLE</Text>
-                <Text>TEST VARIJABLE</Text>
-                <Text>TEST VARIJABLE</Text>
-                <Text>TEST VARIJABLE</Text>
+                <Text>{numbers}</Text>
+                <Text>{names}</Text>
+                <Text>{numbers[2]}</Text>
+                <Text>{names.length}</Text>
+
+                <StatusBar animated backgroundColor="#00f" style="dark" />
             </View>
         );
     }
