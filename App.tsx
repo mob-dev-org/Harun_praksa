a6dde604142856032e98a27cdd2185a8fdc9e47f
harunXh <husejnovic.harun@gmail.com>
2023-02-07 12:56:54 +0100

    feat(objects): add some examples, and test them

--- App.tsx
+++ App.tsx
@@ -9,16 +9,25 @@ import Navigation from './src/navigation';
 export default function App() {
     const isLoadingComplete = useCachedResources();
     const colorScheme = useColorScheme();
+    const ime: { [key: string]: number } = {};
+    ime.Jack = 23;
+    // ime.Bajro = 23; must be string
+    const car: { name: string; km: number; color?: string } = {
+        name: 'audi',
+        km: 115000,
+    };
+    car.km = 112000;
+    car.color = 'blue';
 
     if (!isLoadingComplete) {
         return null;
     } else {
         return (
             <View style={[{ alignItems: 'center', justifyContent: 'center', height: '50%' }]}>
-                <Text>TEST Dev</Text>
-                <Text>TEST Dev</Text>
-                <Text>TEST Dev</Text>
-                <Text>TEST Dev</Text>
+                <Text>{car.name}</Text>
+                <Text>{car.km}</Text>
+                <Text>{car.color}</Text>
+                <Text>{ime.Jack}</Text>
                 <Text>TEST Dev</Text>
             </View>
         );
