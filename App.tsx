e815aa462656c1e749afa54e8926471c46378527
harunXh <husejnovic.harun@gmail.com>
2023-02-13 15:37:01 +0100

    feat(example): add some more array examples

--- App.tsx
+++ App.tsx
@@ -10,13 +10,17 @@ export default function App() {
     let str = 'hello';
     let num: number = 23.123;
 
+    let musicStyle = [' Rock', ',', ' Pop', ', '];
+    musicStyle.push('Jazz');
+    musicStyle[Math.floor((musicStyle.length - 1) / 2)] = 'Sevdah';
+
     return (
         <View style={[{ alignItems: 'center', justifyContent: 'center', height: '50%' }]}>
             <Text>{str.toUpperCase()}</Text>
             <Text>{str.length}</Text>
             <Text>{num.toFixed(1)} </Text>
             <Text>{Math.random().toFixed(3)} </Text>
-            <Text>Test </Text>
+            <Text>{musicStyle} </Text>
             <Text>TEST Dev</Text>
             <Text>TEST Dev</Text>
         </View>
